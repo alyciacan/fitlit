39d1188036ff53233e8c23ba04bd89478882aa46
Alycia Canavan <alycia.canavan@gmail.com>
2022-11-09 12:27:46 -0800

    Update apicalls with endpoints

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,19 +1,19 @@
 const fetchData = (dataFileName, dataKey) => {
-return fetch(`https://fitlit-9ss8e7vre-scottdgaines.vercel.app/api/v1/${dataFileName}`)
-  .then(response => response.json())
-  .then(data => data[dataKey])
-};
-
-const fetchPost = (url, initObject) => {
-  return fetch(`https://fitlit-9ss8e7vre-scottdgaines.vercel.app/api/v1/${url}`, {
-    method: 'POST',
-    headers: {'Content-Type': 'application/json'},
-    body: JSON.stringify(initObject)
-  })
-    .then(response => handleErrors(response))
+  return fetch(`https://fitlit-9ss8e7vre-scottdgaines.vercel.app/api/v1/${dataFileName}`)
     .then(response => response.json())
-    .catch(err => showErrorMessage())
-};
+    .then(data => data[dataKey])
+  };
+  
+  const fetchPost = (url, initObject) => {
+    return fetch(`https://fitlit-9ss8e7vre-scottdgaines.vercel.app${url}`, {
+      method: 'POST',
+      headers: {'Content-Type': 'application/json'},
+      body: JSON.stringify(initObject)
+    })
+      .then(response => handleErrors(response))
+      .then(response => response.json())
+      .catch(err => showErrorMessage())
+  };
 
 function handleErrors(response) {
   if (!response.ok) {
